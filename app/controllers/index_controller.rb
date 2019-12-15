b652f8fbc367e6c0b11a9708b12bad4285b9a155
Martin Fenner <martin.fenner@datacite.org>
2019-12-15 09:28:50 +0100

    Redirect API root URL to documentation datacite/datacite#907

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,10 +1,9 @@
 class IndexController < ApplicationController
-  def index
-    render plain: ENV['SITE_TITLE']
+  def login
+    fail NotImplementedError, "one-time login and session cookies not supported by this service" 
   end
 
-  def login
-    redirect_to :action => "index"
-    # fail NotImplementedError, "one-time login and session cookies not supported by this service" 
+  def routing_error
+    fail AbstractController::ActionNotFound
   end
 end
