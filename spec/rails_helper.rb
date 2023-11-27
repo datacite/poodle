f3604c5c2f236008e227f57e501e5f00b86e6258
Suzanne Vogt <suzanne.vogt@datacite.org>
2023-11-27 11:10:02 -0500

    Cleanup

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -16,14 +16,13 @@ require "rack/test"
 require "maremma"
 
 WebMock.disable_net_connect!(
-  allow: ['codeclimate.com:443'],
-  # allow: ['codeclimate.com:443', ENV['PRIVATE_IP'], ENV['HOSTNAME']],
-  # allow: ['codeclimate.com:443', ENV['PRIVATE_IP'], ENV['HOSTNAME'], 'whirlwind.local', 'lupo_web_1'],
+  allow: ['codeclimate.com:443', ENV['PRIVATE_IP'], ENV['HOSTNAME']],
   allow_localhost: true
 )
 
 VCR.configure do |c|
   mds_token = Base64.strict_encode64("#{ENV['MDS_USERNAME']}:#{ENV['MDS_PASSWORD']}")
+
   c.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   c.hook_into :webmock
   c.ignore_localhost = true
@@ -31,9 +30,6 @@ VCR.configure do |c|
   # c.ignore_request { |request| URI(request.uri).path.start_with("shoulder") }
   c.filter_sensitive_data("<MDS_TOKEN>") { mds_token }
   c.configure_rspec_metadata!
-
-  # c.allow_http_connections_when_no_cassette = true
-  # c.ignore_hosts "codeclimate.com", "api.stage.datacite.org", "whirlwind.local", "lupo_web_1"
 end
 
 RSpec.configure do |config|
@@ -41,9 +37,6 @@ RSpec.configure do |config|
   config.include Rack::Test::Methods, :type => :api
   config.include Rack::Test::Methods, :type => :request
   config.include Rack::Test::Methods, :type => :controller
-
-  # VCR.turn_off!(:ignore_cassettes => true)
-  # WebMock.allow_net_connect!
 
   def app
     Rails.application
